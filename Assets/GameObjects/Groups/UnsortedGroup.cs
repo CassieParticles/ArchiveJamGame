3cf06742759babf73477ddaa77bcaf4e71111fc1
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-18 16:21:41 +0100

    Unsorted group stored stack of all game ojbects, only has top one enabled

--- Assets/GameObjects/Groups/UnsortedGroup.cs
+++ Assets/GameObjects/Groups/UnsortedGroup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -6,14 +7,38 @@ public class UnsortedGroup : MonoBehaviour
 {
     [SerializeField] private ArtefactGroup group;
 
+    private readonly Stack<Artefact> _artefacts = new Stack<Artefact>();
+
+    public Artefact CurrentArtefact => _artefacts.Peek();
+
+    private void Awake()
+    {
+        //Instantiate the artefacts and add to list
+        foreach (var artefact in group.artefacts)
+        {
+            _artefacts.Push(Instantiate(artefact));
+        }
+    }
+
     private void Start()
     {
-        foreach (var a in group.artefacts)
+        //Set up each artefact
+        foreach (var artefact in _artefacts)
         {
-            Artefact artefact = Instantiate(a);
             Group artefactGroup = Group.GetGroup(artefact.Category);
+            if (artefactGroup == null)
+            {
+                Debug.LogError("Group not found: " + artefact.Category.name);
+                continue;
+            }
             //TODO: Add description to group
             //TODO: Randomise positions slightly
+            
+            //Disable the game objects
+            artefact.gameObject.SetActive(false);
         }
+        
+        //Enable the first game object
+        CurrentArtefact.gameObject.SetActive(true);
     }
 }
