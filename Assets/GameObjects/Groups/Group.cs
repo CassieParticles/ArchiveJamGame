76e00e61c5bbd911ae8aed06a930ae7d4a46d699
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-18 15:58:34 +0100

    Group has static function to find group by category

--- Assets/GameObjects/Groups/Group.cs
+++ Assets/GameObjects/Groups/Group.cs
@@ -1,7 +1,24 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 
 public class Group : MonoBehaviour
 {
-    
+    [SerializeField] Category category;
+    public Category Category => category;
+
+    [CanBeNull]
+    public static Group GetGroup(Category category)
+    {
+        Group[] groups = FindObjectsByType<Group>(FindObjectsSortMode.None);
+        foreach (var group in groups)
+        {
+            if (group.Category == category)
+            {
+                return group;
+            }
+        }
+        //ERROR: GROUP NOT FOUND
+        return null;
+    }
 }
