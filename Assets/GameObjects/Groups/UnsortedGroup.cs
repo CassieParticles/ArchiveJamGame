1a4fb632bbc733ab51bd742b41f6d81d60add864
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-18 16:10:57 +0100

    Unsorted group instantiates given artefacts

--- Assets/GameObjects/Groups/UnsortedGroup.cs
+++ Assets/GameObjects/Groups/UnsortedGroup.cs
@@ -1,7 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 public class UnsortedGroup : MonoBehaviour
 {
-    
+    [SerializeField] private ArtefactGroup group;
+
+    private void Start()
+    {
+        foreach (var a in group.artefacts)
+        {
+            Artefact artefact = Instantiate(a);
+            Group artefactGroup = Group.GetGroup(artefact.Category);
+            //TODO: Add description to group
+            //TODO: Randomise positions slightly
+        }
+    }
 }
