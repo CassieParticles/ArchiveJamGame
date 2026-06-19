1b062489ea1e9bf7e80f3e74a2a3527894b9f51e
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-19 14:21:58 +0100

    Added Description UI object

---
+++ Assets/GameObjects/Groups/GroupDescriptionDisplay/DescriptionObject.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GameObjects.Groups.GroupDescriptionDisplay
+{
+    public class DescriptionObject : MonoBehaviour
+    {
+        //Accessors
+        public static DescriptionObject Instance => FindAnyObjectByType<DescriptionObject>(FindObjectsInactive.Include);
+        public bool Active=>gameObject.activeInHierarchy;
+        
+        private TextMeshProUGUI _text;
+
+        public void Describe(Group group)
+        {
+            _text.text = group.GroupDescription.Description;
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            _text.text = "";
+            gameObject.SetActive(false);
+        }
+    }
+}
