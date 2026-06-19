867a0b34ef671c492ea19c59e879769506b715e9
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-19 13:12:11 +0100

    Added group description script

---
+++ Assets/GameObjects/Groups/GroupDescriptionDisplay/GroupDescription.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ArtefactDescription
+{
+    public ArtefactDescription(Artefact artefact)
+    {
+        _name = artefact.name;
+        //TODO: Get description of artefact
+        _description = "TODO";
+    }
+
+    private string _name;
+    private string _description;
+
+    public string Name => (true ? _name : "?????");
+    public string Description => _description;
+}
+
+public class GroupDescription : MonoBehaviour
+{
+    
+}
